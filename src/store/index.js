8dc83d9616bbef579d203d65c892ef26747c1178
Khushi Jain <khushij1011@gmail.com>
2024-05-28 17:29:49 +0530

    netflix card slider

--- src/store/index.js
+++ src/store/index.js
@@ -45,8 +45,8 @@ const getRawData = async (api, genres, paging) => {
             `${api}${paging ? `&page=${i}` : ""}`
         );
         createArrayFromRawData(results, moviesArray, genres);
-        return moviesArray;
     }
+    return moviesArray;
 }
 
 export const fetchMovies = createAsyncThunk("netflix/trending", async ({ type }, thunkApi) => {
