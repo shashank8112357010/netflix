50baf171f09e664556dc7ff3f8ed4728c2813b4f
Khushi Jain <khushij1011@gmail.com>
2024-05-29 17:10:59 +0530

    adding and removing movies from my list page

--- netflix-api/controllers/UserController.js
+++ netflix-api/controllers/UserController.js
@@ -1,13 +1,13 @@
 const User = require("../models/UserModel");
 
-module.exports.addToLikedMovies = async(req, res)=>{
+module.exports.addToLikedMovies = async (req, res) => {
     try {
-        const {email, data} = req.params;
+        const { email, data } = req.body;
         const user = await User.findOne({ email });
-        if(user){
-            const {likedMovies} = user;
-            const movieAlreadyLiked = likedMovies.find(({id}) => (id === data.id));
-            if(!movieAlreadyLiked){
+        if (user) {
+            const { likedMovies } = user;
+            const movieAlreadyLiked = likedMovies.find(({ id }) => (id === data.id));
+            if (!movieAlreadyLiked) {
                 await User.findByIdAndUpdate(
                     user._id,
                     {
@@ -15,31 +15,57 @@ module.exports.addToLikedMovies = async(req, res)=>{
                     },
                     { new: true },
                 );
-            }else return res.json({
+            } else return res.json({
                 msg: "Movie already added to the liked list.",
             });
         }
-        else await User.create({email, likedMovies: [data]});
+        else await User.create({ email, likedMovies: [data] });
         return res.json({
             msg: "Movie added successfully",
         });
     } catch (error) {
         return res.json({
-            msg: "Error adding movie", 
+            msg: "Error adding movie",
         })
     }
 };
 
-module.exports.getLikedMovies = async(req, res)=>{
+module.exports.getLikedMovies = async (req, res) => {
     try {
-        const {email} = req.body;
-        const user = await User.findOne({email});
-        if(user){
-            res.json({msg: "success", movies: user.likedMovies});
-        }else return res.json({msg: "User with given email not found"});
+        const { email } = req.params;
+        const user = await User.findOne({ email });
+        if (user) {
+            res.json({ msg: "success", movies: user.likedMovies });
+        } else return res.json({ msg: "User with given email not found" });
     } catch (error) {
         return res.json({
             msg: "Error fetching movies"
+        })
+    }
+}
+
+module.exports.removeFromLikedMovies = async (req, res) => {
+    try {
+        const { email, movieId } = req.body;
+        const user = await User.findOne({ email });
+        if (user) {
+            const { likedMovies } = user;
+            const movieIndex = likedMovies.findIndex(({ id }) => id === movieId);
+            if (!movieIndex) res.status(400).send({ msg: "Movie not found" });
+            likedMovies.splice(movieIndex, 1);
+
+            await User.findByIdAndUpdate(
+                user._id,
+                {
+                    likedMovies,
+                },
+                { new: true },
+            );
+            return res.json({msg: "Movie Deleted", movies: likedMovies})
+        }
+    } catch (error) {
+        return res.json({
+            msg: "Error deleting movies"
         })
     }
 }
--- netflix-api/routes/UserRoute.js
+++ netflix-api/routes/UserRoute.js
@@ -1,8 +1,9 @@
-const { addToLikedMovies, getLikedMovies } = require("../controllers/UserController");
+const { addToLikedMovies, getLikedMovies, removeFromLikedMovies } = require("../controllers/UserController");
 
 const router = require("express").Router();
 
 router.post("/add", addToLikedMovies);
 router.get("/liked/:email", getLikedMovies);
+router.put("/delete", removeFromLikedMovies);
 
 module.exports = router;
